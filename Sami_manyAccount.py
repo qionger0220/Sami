d7982ef3b64a327f45223e99bcb3589aae7ee793
qionger0220 <dream1234.12@163.com>
2022-02-25 16:31:42 +0800

    Create Sami_manyAccount.py
    青龙面板同时开多个账号使用说明：
    1、首先用浏览器A登录青龙面板，之后手动执行 ”Sami 青龙面板多开“
    2、其次再用浏览器B登录青龙面板，之后手动执行 ”Sami 青龙面板多开“
    3、这时候两个浏览器都已经同时登录成功了。

---
+++ Sami_manyAccount.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*
+#author：Sami 2022-02-25
+#目前只能同事登录两个账号
+#如果点击其中一个账号退出，两个登录的账号会同时退出
+
+'''
+new Env('Sami 青龙面板多开');
+'''
+'''
+1 1 1 1 1 Sami_jd_fcwb.js
+'''
+
+import socket
+import base64
+import json
+import os
+import sys
+import logging
+import time
+import re
+
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+logger = logging.getLogger(__name__)
+logger.debug("\nSami青龙面板 多开账号!\n")
+
+def sami_login():
+    path = '/ql/config/auth.json'
+    if os.path.isfile(path):
+        with open(path, "r") as file:
+            auth1 = file.read()
+            file.close()
+        auth = json.loads(auth1)
+        username = auth["username"]
+        password = auth["password"]
+        lastlogon = auth["lastlogon"]
+        retries = auth["retries"]
+        lastip = auth["lastip"]
+        lastaddr = auth["lastaddr"]
+        platform = auth["platform"]
+        isTwoFactorChecking = auth["isTwoFactorChecking"]
+        token = auth["token"]
+        tokens = auth["tokens"]["desktop"]
+        if ("sami_token" in auth):
+            sami_token = auth["sami_token"]
+            if(sami_token == token):
+                logger.info("程序已经执行过,程序退出\n")
+                sys.exit(1)
+            else:
+                tokens = sami_token
+                sami_token = token
+                logger.info("程序执行成功,可以多开账户啦！")
+        else:
+            sami_token = token
+            logger.info("程序执行成功,可以多开账户啦！")
+        with open(path,"w") as file:   #”w"代表着每次运行都覆盖内容
+            file.write(str('{"username":"'+ str(username)+'","password":"'+ str(password)+ '","token":"'+str(token)+'","tokens":{"desktop":"'+str(tokens)+'"},"lastlogon":'+str(lastlogon)+',"retries":'+str(retries)+',"lastip":"'+str(lastip)+'","lastaddr":"'+str(lastaddr)+'","sami_token":"'+str(sami_token)+'","platform":"'+str(platform)+'","isTwoFactorChecking":'+str(isTwoFactorChecking).lower()+'}'))
+
+if __name__ == '__main__':
+    logger.info("\n--------------------\n")
+    sami_login()
